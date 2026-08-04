5417a647ae24604241cc8efa472dbcd1e010947b
Atif Memon <atifmemon05@icloud.com>
2026-08-04 15:07:36 -0600

    added id's for each button/readable input, and connected them to the js file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // NOTE: Will add onto and finish later
+
+// Locate elements in the DOM (Document Object Model)
+const actionButton = document.getElementById('action-btn');
+const resultBox = document.getElementById('result-box');
+const fileInput = document.getElementById('file-input');
