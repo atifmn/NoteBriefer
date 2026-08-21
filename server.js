523cc93c2f6e93065c60c771dc7a2328df61e5f1
Atif Memon <atifmemon05@icloud.com>
2026-08-20 23:34:22 -0600

    Setup server template for reading and sending file to API/AI

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
+require("dotenv").config();
+
 import { GoogleGenAI } from "@google/genai";
 require("dotenv").config();
 
 const API_KEY = process.env.GEMINI_API_KEY
 const ai = new GoogleGenAI({ apiKey: API_KEY });
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("."));
+
+app.get("/convert", async (req, res) => {
+    try {
+        const result = await //NOTE: FINISH
+
+        res.json({ result });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Open http://localhost:3000");
+});
